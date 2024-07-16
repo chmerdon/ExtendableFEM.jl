b81de2d9a52e2c4e6116342b101b1809d3c9b24f
Christian Merdon <merdon@wias-berlin.de>
2024-07-16 22:54:18 +0200

    added Example330 to doc

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using ExampleJuggler
 using CairoMakie
 using ExtendableFEM
 
-function make_all(; with_examples::Bool = true, run_examples::Bool = true, run_notebooks::Bool = false)
+function make_all(; with_examples::Bool = true, modules = :all, run_examples::Bool = true, run_notebooks::Bool = false)
 
 	module_examples = []
 
@@ -12,41 +12,45 @@ function make_all(; with_examples::Bool = true, run_examples::Bool = true, run_n
 
 		example_dir = joinpath(@__DIR__, "..", "examples")
 
-		modules = [
-			"Example103_BurgersEquation.jl",
-			"Example105_NonlinearPoissonEquation.jl",
-			"Example106_NonlinearDiffusion.jl",
-			"Example108_RobinBoundaryCondition.jl",
-			"Example201_PoissonProblem.jl",
-			"Example202_MixedPoissonProblem.jl",
-			"Example203_PoissonProblemDG.jl",
-			"Example204_LaplaceEVProblem.jl",
-			"Example205_HeatEquation.jl",
-			"Example206_CoupledSubGridProblems.jl",
-			"Example207_AdvectionUpwindDG.jl",
-			"Example210_LshapeAdaptivePoissonProblem.jl",
-			"Example211_LshapeAdaptiveEQPoissonProblem.jl",
-			"Example220_ReactionConvectionDiffusion.jl",
-			"Example225_ObstacleProblem.jl",
-			"Example226_Thermoforming.jl",
-			"Example230_NonlinearElasticity.jl",
-			"Example235_StokesIteratedPenalty.jl",
-			"Example240_SVRTEnrichment.jl",
-			"Example245_NSEFlowAroundCylinder.jl",
-			"Example250_NSELidDrivenCavity.jl",
-			"Example252_NSEPlanarLatticeFlow.jl",
-			"Example260_AxisymmetricNavierStokesProblem.jl",
-			"Example265_FlowTransport.jl",
-			"Example270_NaturalConvectionProblem.jl",
-			"Example275_OptimalControlStokes.jl",
-			"Example280_CompressibleStokes.jl",
-			"Example282_IncompressibleMHD.jl",
-			"Example284_LevelSetMethod.jl",
-			"Example285_CahnHilliard.jl",
-			"Example290_PoroElasticity.jl",
-			"Example301_PoissonProblem.jl",
-			"Example310_DivFreeBasis.jl",
-		]
+		if modules === :all
+			modules = [
+				"Example103_BurgersEquation.jl",
+				"Example105_NonlinearPoissonEquation.jl",
+				"Example106_NonlinearDiffusion.jl",
+				"Example108_RobinBoundaryCondition.jl",
+				"Example201_PoissonProblem.jl",
+				"Example202_MixedPoissonProblem.jl",
+				"Example203_PoissonProblemDG.jl",
+				"Example204_LaplaceEVProblem.jl",
+				"Example205_HeatEquation.jl",
+				"Example206_CoupledSubGridProblems.jl",
+				"Example207_AdvectionUpwindDG.jl",
+				"Example210_LshapeAdaptivePoissonProblem.jl",
+				"Example211_LshapeAdaptiveEQPoissonProblem.jl",
+				"Example220_ReactionConvectionDiffusion.jl",
+				"Example225_ObstacleProblem.jl",
+				"Example226_Thermoforming.jl",
+				"Example230_NonlinearElasticity.jl",
+				"Example235_StokesIteratedPenalty.jl",
+				"Example240_SVRTEnrichment.jl",
+				"Example245_NSEFlowAroundCylinder.jl",
+				"Example250_NSELidDrivenCavity.jl",
+				"Example252_NSEPlanarLatticeFlow.jl",
+				"Example260_AxisymmetricNavierStokesProblem.jl",
+				"Example265_FlowTransport.jl",
+				"Example270_NaturalConvectionProblem.jl",
+				"Example275_OptimalControlStokes.jl",
+				"Example280_CompressibleStokes.jl",
+				"Example281_CompressibleStokesHamiltonian.jl",
+				"Example282_IncompressibleMHD.jl",
+				"Example284_LevelSetMethod.jl",
+				"Example285_CahnHilliard.jl",
+				"Example290_PoroElasticity.jl",
+				"Example301_PoissonProblem.jl",
+				"Example310_DivFreeBasis.jl",
+				"Example330_HyperElasticity.jl",
+			]
+		end
 
 		#notebooks = ["PlutoTemplate.jl"
 		#             "Example with Graphics" => "ExamplePluto.jl"]
